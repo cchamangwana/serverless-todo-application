455fb2fb3e53ead0c78fbc2877854d844350fee7
Chimwemwe Chamangwana <chimwemwechamangwana1@gmail.com>
2022-10-23 15:41:23 +0200

    update models

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 't4ksbi3sjb'
+const apiId = ''
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-rmyj19ww.us.auth0.com',
-  clientId: 'ssZx6966zpzL1rFAioiZq7uqYrvhVIN5',
-  callbackUrl: 'http://localhost:3000/callback'
+  domain: '',
+  clientId: '',
+  callbackUrl: ''
 }
